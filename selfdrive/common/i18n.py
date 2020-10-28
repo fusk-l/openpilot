a2c7f8d33147c4dd29a502f77389ca50db00d1f2
xx979xx <xx979xx@github.com>
2020-10-28 12:52:58 +0300

    update

--- selfdrive/common/i18n.py
+++ selfdrive/common/i18n.py
@@ -1,13 +1,13 @@
 import gettext
 import re
+from common.params import Params
 from common.hardware import HARDWARE, ANDROID, PC
 
 supported_language = ['en-US', 'ar-EG']
 localedir = '../assets/locales'
 
 if ANDROID:
-
-  local = android.getprop(ro.product.locale)
+  local = Params().get('AndroidLocal').decode('utf8')
 else:
   local = supported_language[0]
 
@@ -15,12 +15,12 @@ i18n = gettext.translation('messages', localedir=localedir, fallback=True, langu
 
 i18n.install()
 
-convert_to_symbol = False
-symbol_map = {'ar_EG':{'0':'٠','1':'١','2':'٢','3':'٣','4':'٤','5':'٥','6':'٦','7':'٧','8':'٨','9':'٩'},}
+number_to_symbol = False
+symbol_map = {'ar_EG':['٠','١','٢','٣','٤','٥','٦','٧','٨','٩',],}
 def symble_lookup(re_match):
-  return symbol_map[local][re_match.group(0)]
+  return symbol_map[local][int(re_match.group(0))]
 def numtosym(text):
-  if convert_num and local in symbol_map:
+  if number_to_symbol and local in symbol_map:
     return re.sub("\d", symble_lookup, text)
   else:
     return num
